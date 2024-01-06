0f740e0d5b41419cdedd2cd66a526a5ff39d4920
Kartik-Bankapur <155715998+Kartik-Bankapur@users.noreply.github.com>
2024-01-06 16:51:48 +0000

    variables done

---
+++ Basics/1_variables.js
@@ -0,0 +1,10 @@
+accountName = 'Kartik'   //Undeclared variables
+const accountNumber = 1245 //Const is used declare  unchanging values 
+let password = 99202       //let is used declare changing values
+var pincode = 580024      //var is used earlier which is stopped due to its Global scope and function scope
+
+let accountState; //Undefined variable
+
+accountName;
+
+console.table([accountName,accountNumber,password,pincode,accountState]);
