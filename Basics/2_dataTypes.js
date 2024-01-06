3c2cd0e0abe015df8129df6a5c2332d27578eff2
Kartik-Bankapur <155715998+Kartik-Bankapur@users.noreply.github.com>
2024-01-06 17:24:40 +0000

    datatypes

---
+++ Basics/2_dataTypes.js
@@ -0,0 +1,16 @@
+let name = 'Kartik'; //String
+let age = 18; //Number
+let x = BigInt(1233333333333333333333333333333); //Bigint
+let isAvailable = true //Boolean
+let friendName; //Undefined
+let bankBalance = null; //null
+// Symbol => unique
+
+const fullName = {         //Object
+    firstName:'Kartik',
+    lastName:'Bankapur'
+};
+
+console.log(typeof fullName);
+console.log(typeof null); //Object
+console.log(typeof undefined); //Undefined
